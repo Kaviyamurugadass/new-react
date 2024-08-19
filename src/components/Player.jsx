ac9ba43c4746ee523d2c4d47705b41accfb7a8a8
Kaviya <131757368+Kaviyamurugadass@users.noreply.github.com>
2024-08-19 16:50:21 +0530

    commit4

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -5,14 +5,15 @@ export default function Player() {
   
   function display(){
     setName(Reference.current.value);
+    Reference.current.value = "";
   }
   return (
    
     <section id="player">
       <h2>Welcome {name ?? "unknown entity"}</h2>
       <p>
-        <input type="text" ref={Reference}/>
-        <button onClick={display}>Set Name</button>
+        <input type="text" ref={Reference} placeholder='player name please'/>
+        <button onClick={display}>SET NAME</button>
         
       </p>
       <h1>{name}</h1>
