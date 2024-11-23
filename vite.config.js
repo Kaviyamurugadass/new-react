da97c30094fac0cf94e128a0e50aab27248d0cee
Kaviyamurugadass <kaviyamurugadass2@gmail.com>
2024-11-23 19:56:31 +0530

    Update repository and base URL configurations

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/new-react/',
+  base: '/Timer-Game/',
   plugins: [react()],
   build: {
     cssMinify: true,
