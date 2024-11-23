44154fa83ca7f3ff0258b409a2afcf8b70535333
Kaviyamurugadass <kaviyamurugadass2@gmail.com>
2024-11-23 20:19:12 +0530

    Fix repository paths to match actual repository name

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Timer-Game/',
+  base: '/new-react/',
   plugins: [react()],
   build: {
     cssMinify: true,
